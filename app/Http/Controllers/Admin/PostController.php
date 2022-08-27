95c1ecdfb2e245489815131d1a60194314b03894
erica201193 <erica.mancini20@gmail.com>
2022-08-28 00:38:33 +0200

    Completed

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -10,6 +10,49 @@ use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
+    /* SLUG */
+    private function findBySlug($slug)
+    {
+        $post = Post::where("slug", $slug)->first();
+
+        if (!$post) {
+            abort(404);
+        }
+
+        return $post;
+    }
+
+    private function generateSlug($text)
+    {
+        $toReturn = null;
+        $counter = 0;
+
+        do {
+            // generiamo uno slug partendo dal titolo
+            $slug = Str::slug($text);
+
+            // se il counter é maggiore di 0, concateno il suo valore allo slug
+            if ($counter > 0) {
+                $slug .= "-" . $counter;
+            }
+
+            // controllo a db se esiste già uno slug uguale
+            $slug_esiste = Post::where("slug", $slug)->first();
+
+            if ($slug_esiste) {
+                // se esiste, incremento il contatore per il ciclo successivo
+                $counter++;
+            } else {
+                // Altrimenti salvo lo slug nei dati del nuovo post
+                $toReturn = $slug;
+            }
+        } while ($slug_esiste);
+
+        return $toReturn;
+    }
+
+    /* -------------- I N D E X -------------- */
+
     /**
      * Display a listing of the resource.
      *
@@ -22,6 +65,8 @@ class PostController extends Controller
         return view("admin.posts.index", compact("posts"));
     }
 
+    /* -------------- C R E A T E -------------- */
+
     /**
      * Show the form for creating a new resource.
      *
@@ -32,6 +77,8 @@ class PostController extends Controller
         return view("admin.posts.create");
     }
 
+    /* -------------- S T O R E -------------- */
+
     /**
      * Store a newly created resource in storage.
      *
@@ -40,59 +87,45 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        // Validazione
         $validatedData = $request->validate([
             "title" => "required|min:10",
             "content" => "required|min:10",
         ]);
 
+        // Salvataggio a db
         $post = new Post();
-
         $post->fill($validatedData);
-    
-        //Creazione slug
 
-        $slug = Str::slug($post->title);
-
-        $slug_exist = Post::where("slug", $slug)->first();
-
-
-        $counter = 0;
-        do {
-            $slug = Str::slug($post->title);
-
-            if ($counter > 0) {
-                $slug . "-" . $counter;
-            }
-
-            $slug_exist = Post::where("slug", $slug)->first();
-
-            if($slug_exist) {
-                $counter++;
-            } else {
-                $post->slug = $slug;
-            }
-        }while ($slug_exist);
+        // Creazione slug
+        $post->slug = $this->generateSlug($post->title);
 
         // Salvataggio
         $post->save();
 
-        // Mail::to($post->user->email)->send(new NewPostMail);
-
+        // Redirect alla pagina show
         return redirect()->route("admin.posts.show", $post->id);
+
+
+        // M A I L S
+
+        // Mail::to($post->user->email)->send(new NewPostMail);
     }
 
+    /* -------------- S H O W -------------- */
     /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        $post = Post::findOrFail($id);
-
+        $post = $this->findBySlug($slug);
         return view("admin.posts.show", compact("post"));
     }
+
+    /* -------------- E D I T -------------- */
 
     /**
      * Show the form for editing the specified resource.
@@ -100,12 +133,14 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($slug)
     {
-        $post = Post::findOrFail($id);
+        $post = $this->findBySlug($slug);
 
         return view("admin.posts.edit", compact("post"));
     }
+
+    /* -------------- U P D A T E -------------- */
 
     /**
      * Update the specified resource in storage.
@@ -114,19 +149,38 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $slug)
     {
-        //
+        $validatedData = $request->validate([
+            "title" => "required|min:10",
+            "content" => "required|min:10"
+        ]);
+        $post = $this->findBySlug($slug);
+
+        if ($validatedData["title"] !== $post->title) {
+            // genero un nuovo slug
+            $post->slug = $this->generateSlug($validatedData["title"]);
+        }
+
+        $post->update($validatedData);
+
+        return redirect()->route("admin.posts.show", $post->slug);
     }
 
+     /* -------------- D E S T R O Y -------------- */
+    
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($slug)
     {
-        //
+        $post = $this->findBySlug($slug);
+
+        $post->delete();
+
+        return redirect()->route("admin.posts.index");
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,40 +68,32 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/admin') }}">Admin</a>
+                        <a href="{{ url('/admin/posts') }}">I tuoi Post</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Registrati</a>
                         @endif
                     @endauth
                 </div>
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel PARTE PUBBLICA
-                </div>
-
                 @auth
+                <div class="title m-b-md">
+                    Laravel PARTE PRIVATA
+                </div>
                 <h3>Utente loggato - {{ Auth::user()->name }}</h3>
                 @endauth
 
                 @guest
-                <h3>Utente Non loggato</h3>
+                <div class="title m-b-md">
+                    Laravel PARTE PUBBLICA
+                </div>
+                <h3>Non sei loggato, fai click su Login oppure registrati</h3>
                 @endguest
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
